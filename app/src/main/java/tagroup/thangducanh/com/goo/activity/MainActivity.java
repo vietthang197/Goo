6de7320b3b92ea738f813806c8ebfcbc2a4e4231
Lê Việt Thắng <levietthang1997@gmail.com>
2018-03-26 11:12:28 +0700

    Project verson 1

---
+++ app/src/main/java/tagroup/thangducanh/com/goo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package tagroup.thangducanh.com.goo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tagroup.thangducanh.com.goo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
